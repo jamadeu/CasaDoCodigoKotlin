3d37332e5420cb0f5152ecce05a9638c4755314c
Jean Amadeu <jean.amadeu2@gmail.com>
2021-03-23 13:21:16 -0300

    List

--- src/main/kotlin/br/com/zup/author/AuthorController.kt
+++ src/main/kotlin/br/com/zup/author/AuthorController.kt
@@ -1,8 +1,7 @@
 package br.com.zup.author
 
-import io.micronaut.http.annotation.Body
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Post
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.*
 import io.micronaut.validation.Validated
 import javax.validation.Valid
 
@@ -11,6 +10,22 @@ import javax.validation.Valid
 class AuthorController(
     val authorRepository: AuthorRepository
 ) {
+
+    @Get
+    fun list(@QueryValue(defaultValue = "") email: String): HttpResponse<Any> {
+        if (email.isEmpty()) {
+            val authorList = authorRepository.findAll();
+            val response = authorList.map { author -> AuthorResponse(author) }
+            return HttpResponse.ok(response)
+        }
+
+        val optional = authorRepository.findByEmail(email);
+        if (optional.isEmpty) {
+            return HttpResponse.notFound()
+        }
+
+        return HttpResponse.ok(AuthorResponse(optional.get()))
+    }
 
     @Post
     fun create(@Body @Valid request: NewAuthorRequest) {
--- src/main/kotlin/br/com/zup/author/AuthorRepository.kt
+++ src/main/kotlin/br/com/zup/author/AuthorRepository.kt
@@ -2,7 +2,9 @@ package br.com.zup.author
 
 import io.micronaut.data.annotation.Repository
 import io.micronaut.data.jpa.repository.JpaRepository
+import java.util.*
 
 @Repository
 interface AuthorRepository : JpaRepository<Author, Long> {
+    fun findByEmail(email: String): Optional<Author>
 }
